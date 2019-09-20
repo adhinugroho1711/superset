27dfc6ccb18253c52a9fe00681ab7ea9e6ef6103
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-20 17:54:15 +0700

    Update install-superset.sh

--- install-superset.sh
+++ install-superset.sh
@@ -1,3 +1,10 @@
 ##wget https://raw.githubusercontent.com/helm/charts/master/stable/superset/values.yaml
+snap install helm --classic
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --upgrade
+
+sleep 2m
+
 wget https://raw.githubusercontent.com/phenom1711/superset/master/superset-value.yaml
 helm install --name superset -f superset-values.yaml stable/superset
