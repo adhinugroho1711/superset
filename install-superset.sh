10ad846bf0730fc1efa1c8846e3090f841c45653
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-23 08:07:31 +0700

    Update install-superset.sh

--- install-superset.sh
+++ install-superset.sh
@@ -1,4 +1,4 @@
 ##wget https://raw.githubusercontent.com/helm/charts/master/stable/superset/values.yaml
 #wget https://raw.githubusercontent.com/phenom1711/superset/master/superset-value.yaml
-kubectl create configmap superset-config --from-file superset_config.py 
+#kubectl create configmap superset-config --from-file superset_config.py 
 helm install --name superset -f superset-value.yaml stable/superset
